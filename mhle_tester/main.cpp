cd77c5ea66a71ca7c5ebf83bc7d369fa78a9e8d2
WanYaoWen <WanYaoWen@outlook.com>
2020-08-20 21:10:00 +0800

    修正一小问题

--- mhle_tester/main.cpp
+++ mhle_tester/main.cpp
@@ -2,7 +2,7 @@
 #include"../libmhle/solver.h"
 using namespace std;
 int main() {
-	int num,a;
+	int num;
 	vector<mhle::fraction> vec, ans;
 	cout << "请输入方程未知数的数量：";
 	cin >> num;
@@ -10,10 +10,10 @@ int main() {
 	for (int i = 1; i <= num; i++) {
 		cout << "请输入第 " << i << " 个方程（格式：若未知数数量为3，2a-b=1 => 2 -1 0 1）：" << endl;
 		vec.clear();
-		string tmp;
+		mhle::fraction tmp;
 		for (int i = 0; i < num + 1; i++) {
 			cin >> tmp;
-			vec.push_back(mhle::fraction(tmp));
+			vec.push_back(tmp);
 		}
 		try {
 			solver.add_equations(vec.begin(), vec.end());
